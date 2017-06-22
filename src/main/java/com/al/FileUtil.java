bc2ee717ad28094ac3e7230481f49f4b17643c50
Aravindan Thulasinathan <aravindan.thulasinathan@oracle.com>
2017-06-22 11:35:53 -0700

    moving packages

--- src/main/java/com/al/FileUtil.java
+++ src/main/java/com/al/FileUtil.java
@@ -1,20 +1,4 @@
-/*************************************************************************
- * ORACLE CONFIDENTIAL
- * __________________
- * <p/>
- * [2014] - [2016] Oracle Incorporated
- * All Rights Reserved.
- * <p/>
- * NOTICE:  All information contained herein is, and remains
- * the property of Oracle Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Oracle Incorporated
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Oracle Incorporated.
- */
+package com.al;
 
 import java.io.*;
 import java.util.ArrayList;
